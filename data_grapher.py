0e664610583abfaa991c385d0c350030d47b89a2
xiaodongbei <1096242371@qq.com>
2023-07-22 22:23:49 +0800

    [crawl_script]: update crawl script

--- data_grapher.py
+++ data_grapher.py
@@ -6,7 +6,9 @@ from matplotlib import pyplot as plt
 from matplotlib import font_manager
 
 block_map = {
-    "静安": ["不夜城", "曹家渡", "大宁", "江宁路", "静安寺", "南京西路", "彭浦", "西藏北路", "阳城", "永和", "闸北公园"]
+    "静安": ["不夜城", "曹家渡", "大宁", "江宁路", "静安寺", "南京西路", "彭浦", "西藏北路", "阳城", "永和", "闸北公园"],
+    "黄浦": ["打浦桥", "董家渡", "淮海中路", "黄浦滨江", "老西门", "南京东路", "蓬莱公园", "人民广场", "世博滨江", "五里桥", "新天地", "豫园"],
+    "虹口": ["江湾镇", "凉城", "临平路", "鲁迅公园", "曲阳", "四川北路"]
     }
 
 class Grapher:
@@ -14,6 +16,7 @@ class Grapher:
         self.dataset = dataset
         self.parser = Parser(dataset=dataset)
         self.dirname = os.path.dirname(dataset)
+        self.block = self.dirname.split("\\")[-1].split("_")[0]
 
     def display_house_num_from_diff_block(self):
         block_dict = {"其它": 0}
@@ -22,7 +25,7 @@ class Grapher:
             block = self.parser.get_block_from_table(table)
             if block in block_dict:
                 block_dict[block] += 1
-            elif block not in block_map["静安"]:
+            elif block not in block_map[self.block]:
                 block_dict["其它"] += 1
             else:
                 block_dict[block] = 1
@@ -46,7 +49,7 @@ class Grapher:
             if block in block_dict:
                 block_dict[block][0] += 1
                 block_dict[block][1] += unit_price
-            elif block not in block_map["静安"]:
+            elif block not in block_map[self.block]:
                 block_dict["其它"][0] += 1
                 block_dict["其它"][1] += unit_price
             else:
@@ -62,11 +65,7 @@ class Grapher:
         plt.bar_label(plot, label_type="edge")
         plt.show()
 
-
     def run(self):
-        # self.display_house_num_from_diff_block()
+        self.display_house_num_from_diff_block()
         self.display_house_unit_price_from_diff_block()
         self.parser.teardown()
-
-dataset_path = "E:\\WorkSpace\\crawl_script\\data\\shanghai\\20230722175151\\20230722175151.db"
-Grapher(dataset=dataset_path).run()
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import os
+import sys
 import argparse
 
 from data_crawler import Crawl
+from data_grapher import Grapher
 
 
 area_map = {
@@ -17,20 +20,33 @@ area_map = {
 
 
 class Crawler:
-    def __init__(self, city, timestamp, area, operation):
+    def __init__(self, city, timestamp, area, operation, dataset):
         self.city = city
         self.timestamp = timestamp
         self.area = area
         self.operation = operation
         self.crawler = Crawl(city=city, timestamp=timestamp, area=area)
-        
+        self.dataset = dataset
+        self.env_init()
+    
+    def env_init(self):
+        if (self.operation == "graph") and (not self.dataset):
+            print("Operation is graph but dataset is None")
+            sys.exit(1)
+
     def run(self):
         if self.operation == "crawl":
+            print("")
             self.crawler.run()
-
-
-
-
+        elif self.operation == "parse":
+            pass
+        elif self.operation == "graph":
+            if os.path.exists(self.dataset):
+                self.grapher = Grapher(dataset=self.dataset)
+                self.grapher.display_house_num_from_diff_block()
+                self.grapher.display_house_unit_price_from_diff_block()
+            else:
+                print(f"{self.dataset} is not existed!")
 
 
 if __name__ == "__main__":
@@ -50,7 +66,7 @@ if __name__ == "__main__":
         dest="area",
         type=str,
         default="全部区域",
-        choices=["浦东", "闵行", "宝山", "徐汇", "普陀", "杨浦", "长宁", "松江", "嘉定", "黄埔", "静安", "虹口", "青浦", "奉贤", "金山", "崇明"],
+        choices=["浦东", "闵行", "宝山", "徐汇", "普陀", "杨浦", "长宁", "松江", "嘉定", "黄浦", "静安", "虹口", "青浦", "奉贤", "金山", "崇明"],
         help="Specify area to crawl, default crawl all"
         )
     parser.add_argument(
@@ -70,4 +86,20 @@ if __name__ == "__main__":
         choices=["crawl", "parse", "graph"],
         help="Specify operation to run, default crawl data"
         )
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        dest="dataset",
+        type=str,
+        default="",
+        help="Specify dataset to parse, default skip"
+        )
+
     args = parser.parse_args()
+    Crawler(
+        city=args.city,
+        area=args.area,
+        timestamp=args.timestamp,
+        operation=args.operation,
+        dataset=args.dataset
+        ).run()
